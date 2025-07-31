fda9462c7dd177407d360ba15eb6a547350559a8
TungYuChiang <jokkn5423@gmail.com>
2025-07-31 09:15:32 +0800

    Fix: re-add WeSplit as a normal folder

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by TungYu on 2025/7/30.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplit/WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by TungYu on 2025/7/30.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
