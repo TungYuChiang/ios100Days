039a9bbb7e8e9538fa1cb36429ef9752f54a459b
TungYuChiang <jokkn5423@gmail.com>
2025-08-01 16:15:35 +0800

    Day 21

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,18 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by TungYu on 2025/8/1.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
