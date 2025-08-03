595a8f8fdb582dddb2996e3dd8afcb59b3dfb5e9
TungYuChiang <jokkn5423@gmail.com>
2025-08-03 08:54:54 +0800

    Day23~28 done

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by TungYu on 2025/8/2.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
